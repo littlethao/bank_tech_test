d3d9253dfe8e616e08ee174e66e5f930299ce570
Thao Vo <ttthao.vo@gmail.com>
2016-10-18 11:50:07 +0100

    passed tests for new method #deposit(amount) to implement functionality for depositing money to bank account

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,6 +6,10 @@ INITIAL_BALANCE = 0
     @balance = INITIAL_BALANCE
   end
 
+  def deposit(amount)
+    @balance += amount
+  end
+
 private
 
 attr_reader :balance
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,7 +1,21 @@
 require 'bank_account'
 
 describe BankAccount do
-  it 'knows the initial balance of bank account' do
-    expect(subject.balance).to eq(0)
+  subject(:bank_account) { described_class.new }
+
+  context '#initialize' do
+    it 'knows the initial balance of bank account is 0' do
+      expect(bank_account.instance_variable_get(:@balance)).to eq BankAccount::INITIAL_BALANCE
+    end
+  end
+
+  context '#deposit' do
+    it 'knows a deposit' do
+      expect(bank_account).to respond_to(:deposit).with(1).argument
+    end
+
+    it 'knows to adds up balance when money is deposited to bank account' do
+      expect{ bank_account.deposit(1000) }.to change{ bank_account.instance_variable_get(:@balance) }.by 1000
+    end
   end
 end
