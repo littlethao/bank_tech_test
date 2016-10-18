a7dc494908d1860d346c2a148f783a7d6f451d7a
Thao Vo <ttthao.vo@gmail.com>
2016-10-18 11:22:09 +0100

    set default balance for bank account to 0, passed test of #balance, refactored

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,13 @@
 class BankAccount
 
+INITIAL_BALANCE = 0
+
+  def initialize
+    @balance = INITIAL_BALANCE
+  end
+
+private
+
+attr_reader :balance
+
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,5 +1,7 @@
 require 'bank_account'
 
 describe BankAccount do
-
+  it 'knows the initial balance of bank account' do
+    expect(subject.balance).to eq(0)
+  end
 end
